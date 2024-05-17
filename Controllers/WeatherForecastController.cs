883eeb2da2abf2f800ed548213d2f6abd01edfb2
Abhiram13 <abhiramnagadi@gmail.com>
2024-05-17 13:04:14 +0530

    added test api

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -29,4 +29,10 @@ public class WeatherForecastController : ControllerBase
         })
         .ToArray();
     }
+
+    [HttpGet("api")]
+    public string Api()
+    {
+        return "This API is working";
+    }
 }
