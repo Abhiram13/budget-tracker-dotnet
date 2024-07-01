2006bbd9d93820b365f1b3f9e60fb7a2bb5287e0
Abhiram13 <abhiramnagadi@gmail.com>
2024-07-01 21:28:43 +0530

    removed extra spaces

--- Services/Database.service.cs
+++ Services/Database.service.cs
@@ -1,14 +1,11 @@
 using MongoDB.Driver;
-using MongoDB.Bson.Serialization.Attributes;
 using Defination;
-using System;
-using Microsoft.VisualBasic;
 
 namespace Services;
 
 public static class Mongo
 {   
-    private static readonly string url = $"mongodb+srv://{Env.USERNAME}:{Env.PASSWORD}@{Env.HOST}/?retryWrites=true&w=majority&appName=Trsnactions";    
+    private static readonly string url = $"mongodb+srv://{Env.USERNAME}:{Env.PASSWORD}@{Env.HOST}/?retryWrites=true&w=majority&appName=Trsnactions";
     private static readonly MongoClient client = new MongoClient(url);
-    public static readonly IMongoDatabase DB = client.GetDatabase(Env.DB);    
+    public static readonly IMongoDatabase DB = client.GetDatabase(Env.DB);
 }
