8974f0769b102b821348a42b5fdc3f4c7447ac2b
Abhiram13 <abhiramnagadi@gmail.com>
2025-03-14 20:46:32 +0530

    Test data and defination refactor Removed JWT bearer nuget package

---
+++ IntegrationTests/Utilis.cs
@@ -0,0 +1,57 @@
+using System.Text;
+using System.Text.Json;
+using BudgetTracker.Defination;
+using MongoDB.Driver;
+
+namespace IntegrationTests.Utils
+{
+    public class TestDisposal : IAsyncDisposable
+    {
+        protected readonly MongoDBFixture _fixture;
+
+        public TestDisposal(MongoDBFixture fixture)
+        {
+            _fixture = fixture;
+        }
+        
+        public async virtual ValueTask DisposeAsync()
+        {
+            await _fixture.Database.GetCollection<Transaction>("transactions").DeleteManyAsync(FilterDefinition<Transaction>.Empty);
+        }
+    }
+
+    public class TransactionsInsertDisposals : TestDisposal
+    {
+        private readonly HttpClient _httpClient;
+        
+        public TransactionsInsertDisposals(MongoDBFixture fixture, HttpClient httpClient) : base(fixture)
+        {
+            _httpClient = httpClient;
+        }
+        
+        private Transaction[] GetTransactionsPayload()
+        {
+            string categoryId = "665aa29b930ad7888c6766fa";
+            string bankId = "66483fed6c7ed85fca653d05";
+            string date = "2024-09-18";
+            string description = "Sample test transaction";
+
+            return new Transaction[] {
+                new () { Amount = 123, CategoryId = categoryId, Date = date, Description = description, Due = false, FromBank = bankId, ToBank = "", Type = TransactionType.Debit },
+                new () { Amount = 123, CategoryId = categoryId, Date = date, Description = description, Due = false, FromBank = bankId, ToBank = "", Type = TransactionType.Debit },
+                new () { Amount = 234, CategoryId = categoryId, Date = DateTime.Now.ToString("yyyy-MM-dd"), Description = description, Due = false, FromBank = bankId, ToBank = "", Type = TransactionType.Debit },
+            };
+        }
+        
+        public async Task InsertManyAsync()
+        {
+            foreach (Transaction transaction in GetTransactionsPayload())
+            {
+                string json = JsonSerializer.Serialize(transaction);
+                StringContent? payload = new StringContent(json, Encoding.UTF8, "application/json");
+                await _httpClient.PostAsync("transactions", payload);
+            }
+        }
+    }
+}
+
