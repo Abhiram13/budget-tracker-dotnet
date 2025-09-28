f751aea9a2fbca8b7f0b03e2838de2ee0b6f3af0
Abhiram13 <abhiramnagadi@gmail.com>
2025-09-28 12:00:48 +0530

    deleted old budget-tracker project

--- Budget-Tracker/Services/Bank.service.cs
+++
@@ -1,9 +0,0 @@
-using BudgetTracker.Defination;
-using BudgetTracker.Interface;
-
-namespace BudgetTracker.Services;
-
-public class BankService : MongoServices<Bank>, IBankService
-{
-    public BankService(IMongoContext mongoContext) : base(mongoContext.Bank) { }
-}
