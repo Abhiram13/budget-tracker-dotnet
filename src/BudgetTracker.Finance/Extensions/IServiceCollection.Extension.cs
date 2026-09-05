2428108664eeeeac8db9a8d66329e89b130e8928
Abhiram13 <abhiramnagadi@gmail.com>
2026-09-05 20:52:19 +0530

    refactor: file name changes, service collection and web application extension updates

---
+++ src/BudgetTracker.Finance/Extensions/IServiceCollection.Extension.cs
@@ -0,0 +1,86 @@
+using System.Net;
+using Abhiram.Secrets.Providers;
+using Abhiram.Secrets.Providers.Interface;
+using BudgetTracker.Workers;
+using BudgetTracker.Interfaces;
+using BudgetTracker.Services;
+using BudgetTracker.ValueObject;
+using BudgetTracker.Context;
+using BudgetTracker.Repository;
+using BudgetTracker.Security;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace BudgetTracker.Extensions;
+
+public static class ServiceCollectionExtension
+{
+    extension(IServiceCollection serviceCollection)
+    {
+        public IServiceCollection AddServiceCollection()
+        {
+            serviceCollection
+                .AddDependencyServices()
+                .AddControllerConfiguration()
+                .AddAuthConfiguration()
+                .AddApplicationServices();
+
+            return serviceCollection;
+        }
+        
+        private IServiceCollection AddDependencyServices()
+        {
+            serviceCollection.AddScoped<CategoryService>();
+            serviceCollection.AddScoped<TransactionService>();
+            serviceCollection.AddScoped<BankService>();
+            serviceCollection.AddScoped<DueService>();
+            serviceCollection.AddSingleton<AppSecrets>();
+            serviceCollection.AddHostedService<SecretHostService>();
+            serviceCollection.AddSingleton<ICategoryRepository, CategoryRepository>();
+            serviceCollection.AddSingleton<IBankRepository, BankRepository>();
+            serviceCollection.AddSingleton<ITransactionRepository, TransactionRepository>();
+            serviceCollection.AddSingleton<IDueRepository, DueRepository>();
+            serviceCollection.AddSingleton<IMongoContext, MongoDBContext>();
+            serviceCollection.AddSingleton<ISecretManager, SecretManagerService>();
+
+            return serviceCollection;
+        }
+
+        private IServiceCollection AddControllerConfiguration()
+        {
+            serviceCollection.AddControllers().ConfigureApiBehaviorOptions(options =>
+            {
+                options.SuppressModelStateInvalidFilter = false;
+                options.InvalidModelStateResponseFactory = action =>
+                {
+                    KeyValuePair<string, ModelStateEntry?> modelState = action.ModelState.FirstOrDefault();
+                    string errorAt = modelState.Key;
+                    string errorMessage = modelState.Value?.Errors?[0].ErrorMessage ?? $"Something went wrong at {errorAt}";
+                    return new BadRequestObjectResult(new ApiResponse<string> { Message = errorMessage, StatusCode = HttpStatusCode.BadRequest });
+                };
+            });
+
+            return serviceCollection;
+        }
+
+        private IServiceCollection AddAuthConfiguration()
+        {
+            serviceCollection.AddAuthentication()
+                .AddScheme<ApiKeySchemaOptions, ApiKeyHandler>(ApiKeySchemaOptions.DefaultSchema, _ => { });
+
+            return serviceCollection;
+        }
+
+        private IServiceCollection AddApplicationServices()
+        {
+            serviceCollection
+                .AddRouting()
+                .AddEndpointsApiExplorer()
+                .AddMemoryCache()
+                .AddCors(opt => opt.AddDefaultPolicy(pol => pol.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()))
+                .AddHealthChecks();
+
+            return serviceCollection;
+        }
+    }
+}
--- src/BudgetTracker.Finance/Program.cs
+++ src/BudgetTracker.Finance/Program.cs
@@ -1,54 +1,13 @@
 using System.Net;
 using Abhiram.Abstractions.Logging;
 using Abhiram.Extensions.DotEnv;
-using Abhiram.Secrets.Providers;
-using Abhiram.Secrets.Providers.Interface;
-using BudgetTracker.Middlewares;
-using BudgetTracker.Workers;
-using BudgetTracker.Interfaces;
-using BudgetTracker.Services;
-using BudgetTracker.ValueObject;
-using BudgetTracker.Context;
-using BudgetTracker.Repository;
-using BudgetTracker.Security;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
+using BudgetTracker.Extensions;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 DotEnvironmentVariables.Load();
 
 builder.AddConsoleGoogleSeriLog();
-builder.Services.AddRouting();
-builder.Services.AddScoped<CategoryService>();
-builder.Services.AddScoped<TransactionService>();
-builder.Services.AddScoped<BankService>();
-builder.Services.AddScoped<DueService>();
-builder.Services.AddSingleton<ICategoryRepository, CategoryRepository>();
-builder.Services.AddSingleton<IBankRepository, BankRepository>();
-builder.Services.AddSingleton<ITransactionRepository, TransactionRepository>();
-builder.Services.AddSingleton<IDueRepository, DueRepository>();
-builder.Services.AddSingleton<IMongoContext, MongoDBContext>();
-builder.Services.AddSingleton<ISecretManager, SecretManagerService>();
-builder.Services.AddSingleton<AppSecrets>();
-builder.Services.AddHostedService<SecretHostService>();
-builder.Services.AddAuthentication().AddScheme<ApiKeySchemaOptions, ApiKeyHandler>(ApiKeySchemaOptions.DefaultSchema, _ => { });
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddMemoryCache();
-builder.Services.AddHealthChecks();
-builder.Services.AddCors(opt => opt.AddDefaultPolicy(pol => pol.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
-builder.Services.AddControllers().ConfigureApiBehaviorOptions(options =>
-{
-    options.SuppressModelStateInvalidFilter = false;
-    options.InvalidModelStateResponseFactory = action =>
-    {
-        KeyValuePair<string, ModelStateEntry?> modelState = action.ModelState.FirstOrDefault();
-        string errorAt = modelState.Key;
-        string errorMessage = modelState.Value?.Errors?[0].ErrorMessage ?? $"Something went wrong at {errorAt}";
-        return new BadRequestObjectResult(new ApiResponse<string> { Message = errorMessage, StatusCode = HttpStatusCode.BadRequest });
-    };
-});
+builder.Services.AddServiceCollection();
 
 builder.WebHost.ConfigureKestrel((_, server) => {
     string portNumber = Environment.GetEnvironmentVariable("PORT") ?? "3000";
@@ -58,20 +17,7 @@ builder.WebHost.ConfigureKestrel((_, server) => {
 
 WebApplication app = builder.Build();
 
-app.UseAuthentication();
-app.UseAuthorization();
-app.UseHttpsRedirection();
-app.MapHealthChecks("/health", new HealthCheckOptions () {
-    ResultStatusCodes = {
-        [HealthStatus.Healthy] = StatusCodes.Status200OK,
-        [HealthStatus.Unhealthy] = StatusCodes.Status200OK,
-        [HealthStatus.Degraded] = StatusCodes.Status500InternalServerError,
-    },
-});
-app.UseMiddleware<ExceptionHandlerMiddleware>();
-app.UseMiddleware<TraceIdMiddleware>();
-app.UseCors();
-app.MapControllers();
+app.UseApplicationServices();
 app.Run();
 
 public partial class Program { }
